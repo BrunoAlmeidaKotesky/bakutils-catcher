d987b6394f96c1c776098b978107cd54698aaeec
Bruno Kotesky <bruno.kotesky@trentim.com>
2023-09-03 19:27:17 -0300

    1.2.2

---
+++ dist/index.d.ts
@@ -0,0 +1,108 @@
+/** Represents a failed computation.*/
+export interface Left<T, E> {
+    type: 'error';
+    error: E;
+    /*** Returns the value of the Result if it is successful, otherwise throws an error.*/
+    unwrap(): T;
+    /*** Returns the value of the Result if it is successful, otherwise returns the provided default value.*/
+    unwrapOr(defaultValue: T): T;
+    /*** Returns the value of the Result if it is successful, otherwise calls the provided function with the error and returns its result.*/
+    unwrapOrElse(fn: (error: E) => T): T;
+    /*** Returns true if the Result is an error, false otherwise.*/
+    isErr(this: Result<T, E>): this is Left<T, E>;
+    /*** Returns true if the Result is successful, false otherwise.*/
+    isOk(this: Result<T, E>): this is Right<T, E>;
+}
+/** Represents a successful computation.*/
+export interface Right<T, E> {
+    type: 'ok';
+    value: T;
+    /*** Returns the value of the Result.*/
+    unwrap(): T;
+    /*** Returns the value of the Result.*/
+    unwrapOr(defaultValue: T): T;
+    /*** Returns the value of the Result.*/
+    unwrapOrElse(fn: (error: E) => T): T;
+    /*** Returns true if the Result is an error, false otherwise.*/
+    isErr(this: Result<T, E>): this is Left<T, E>;
+    /*** Returns true if the Result is successful, false otherwise.*/
+    isOk(this: Result<T, E>): this is Right<T, E>;
+}
+/**Represents the result of a computation that can either succeed with a value of type T or fail with an error of type E.*/
+export type Result<T, E> = Left<T, E> | Right<T, E>;
+/**
+ * Represents an Option that contains a value.
+ */
+export interface SomeType<T> {
+    type: 'some';
+    value: T;
+    /*** Returns the value of the Option if it exists, otherwise throws an error.*/
+    unwrap(): T;
+    /*** Returns the value of the Option if it exists, otherwise returns the provided default value.*/
+    unwrapOr(defaultValue: T): T;
+    /*** Returns the value of the Option if it exists, otherwise calls the provided function and returns its result.*/
+    unwrapOrElse(fn: () => T): T;
+    /*** Returns true if the Option contains a value, false otherwise.*/
+    isSome(this: Option<T>): this is SomeType<T>;
+    /*** Returns true if the Option does not contain a value, false otherwise.*/
+    isNone(this: Option<T>): this is NoneType;
+}
+/**
+ * Represents an Option that does not contain a value.
+ */
+export interface NoneType {
+    type: 'none';
+    /*** Throws an error because None does not contain a value.*/
+    unwrap(): never;
+    /*** Returns the provided default value because None does not contain a value.*/
+    unwrapOr<T>(defaultValue: T): T;
+    /*** Calls the provided function and returns its result because None does not contain a value.*/
+    unwrapOrElse<T>(fn: () => T): T;
+    /*** Returns true if the Option contains a value, false otherwise.*/
+    isSome<T>(this: Option<T>): this is SomeType<T>;
+    /*** Returns true if the Option does not contain a value, false otherwise.*/
+    isNone<T>(this: Option<T>): this is NoneType;
+}
+/**
+ * Represents an optional value: every Option is either Some with a value of type T or None.
+ */
+export type Option<T> = SomeType<T> | NoneType;
+/** Creates a successful Result with the given value.
+ * @param value The value of the successful computation.
+ * @returns A Result with the 'ok' type and the provided value.*/
+export declare function Ok<T, E>(value: T): Result<T, E>;
+/**
+ * Creates a failed Result with the given error.
+ * @param error The error that caused the computation to fail.
+ * @returns A Result with the 'error' type and the provided error.
+ */
+export declare function Err<T, E>(error: E): Result<T, E>;
+/**
+ * Creates an Option with a value.
+ * @param value The value to be wrapped in the Option.
+ * @returns An Option with the 'some' type and the provided value.
+ */
+export declare function Some<T>(value: T): Option<T>;
+/**
+ * Represents an empty Option with no value.
+ * @returns An Option with the 'none' type.
+ */
+export declare const None: Option<never>;
+export declare function isPromise<T = any>(object: any): object is Promise<T>;
+export declare function isFunction(func: any): func is Function;
+export type Handler<R = any, E = any, Args extends any[] = any[], C = any> = (err: E, context: C, ...args: Args) => R;
+/**
+ * Catch decorator: A TypeScript decorator that wraps a class method with error handling logic.
+ * It catches errors of a specific type that are thrown within the decorated method.
+ * @param ErrorClassConstructor - The constructor of the specific error type to catch.
+ * @param handler - The function to handle the caught error.
+ * @returns A decorator function.
+ */
+export declare function Catch<R = any, E = any, Args extends any[] = any[], C = any>(ErrorClassConstructor: Function, handler: Handler<R, E, Args, C>): (_target: any, _key: string, descriptor: PropertyDescriptor) => PropertyDescriptor;
+/**
+ * DefaultCatch decorator: A TypeScript decorator that wraps a class method with error handling logic.
+ * It catches all errors that are thrown within the decorated method.
+ * @param handler - The function to handle the caught error.
+ * @returns A decorator function.
+ */
+export declare function DefaultCatch<R = any, E = any, Args extends any[] = any[], C = any>(handler: Handler<R, E, Args, C>): (_target: any, _key: string, descriptor: PropertyDescriptor) => PropertyDescriptor;
